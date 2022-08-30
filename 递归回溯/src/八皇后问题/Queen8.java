f77372e412d587dde166c7ae5177ac87b7f4d7d7
ghq <gonghanqi2108@yundasys.com>
2022-08-30 18:00:49 +0800

    八皇后

--- 递归回溯/src/八皇后问题/Queen8.java
+++ 递归回溯/src/八皇后问题/Queen8.java
@@ -6,29 +6,45 @@ package 八皇后问题;
  * @Description
  */
 public class Queen8 {
+    //如何记录一个格子里是否有皇后，一种方式是queen[row]=col
+    //另一种方式是使用三个boolean数组
     int[] queen;//queen[row]=col:表示在row行col列有皇后
     int ways;
+    boolean[] cols;//这个用来记录某一列上是不是有皇后cols[col]=true表示这一行有皇后
+    boolean[] rightTop;
+    boolean[] lefttTop;
     public static void main(String[] args) {
-        new Queen8().eightQueen(8);
+        new Queen8().NQueen(8);
     }
-    public void eightQueen(int n){
+    public void NQueen(int n){
+        if(n<1) {return;}
         queen=new int[n];
         //然后一行一行添加皇后
-
-           place(0,n);
+           place(0);
+        System.out.println("一共有"+ways+"方法");
 
     }
-    public void place(int row,int n){
+    public void place(int row){
         if(row== queen.length){
             ways++;
             return;
         }
-         for(int col=0;col<n;col++){
+         for(int col=0;col<queen.length;col++){
              //往这一行放皇后，看看这一行能不能放
-             if(isValid(row,col)){
-                queen[row]=col;
-                place(row+1,n);
-             }
+             if(cols[col]){//说明这一列有皇后
+                 continue; }
+             if(rightTop[row+col+ queen.length-1])
+             {continue;}
+             if(lefttTop[row+col])
+             {continue;}
+             cols[col]=true;
+             rightTop[row+col+ queen.length-1]=true;
+             lefttTop[row+col]=true;
+             place(row+1);
+             cols[col]=false;
+             rightTop[row+col+ queen.length-1]=false;
+             lefttTop[row+col]=false;
+
          }
     }
     public boolean isValid(int row,int col){
@@ -47,4 +63,5 @@ public class Queen8 {
         return true;
 
 }
+//上面这个方法要有优化，没必要每次都循环上面的row一遍
 }
