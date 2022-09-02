e3b606af93944b9ae7f3ef13b37c95ac63765c49
ghq <gonghanqi2108@yundasys.com>
2022-09-02 21:42:20 +0800

    八皇后

--- 递归回溯/src/八皇后问题/Queen8.java
+++ 递归回溯/src/八皇后问题/Queen8.java
@@ -10,15 +10,20 @@ public class Queen8 {
     //另一种方式是使用三个boolean数组
     int[] queen;//queen[row]=col:表示在row行col列有皇后
     int ways;
-    boolean[] cols;//这个用来记录某一列上是不是有皇后cols[col]=true表示这一行有皇后
-    boolean[] rightTop;
+    boolean[] cols;//这个用来记录某一列上是不是有皇后cols[col]=true表示这一行有皇后；继续优化，直接将一个boolean[8]优化为一个byte
+    boolean[] rightTop;//用两个字节取代rightTop和leftTop
     boolean[] lefttTop;
     public static void main(String[] args) {
-        new Queen8().NQueen(8);
+        new Queen8().NQueen(4);
+        System.out.println(Double.SIZE);//Integer4个字节  Byte 1个字节.Double 8个字节
+        System.out.println();
     }
     public void NQueen(int n){
         if(n<1) {return;}
         queen=new int[n];
+        cols=new boolean[(n<<1)-1];
+        rightTop=new boolean[(n<<1)-1];
+        lefttTop=new boolean[(n<<1)-1];
         //然后一行一行添加皇后
            place(0);
         System.out.println("一共有"+ways+"方法");
@@ -27,23 +32,26 @@ public class Queen8 {
     public void place(int row){
         if(row== queen.length){
             ways++;
+            show();
+            System.out.println();
             return;
         }
          for(int col=0;col<queen.length;col++){
              //往这一行放皇后，看看这一行能不能放
              if(cols[col]){//说明这一列有皇后
                  continue; }
-             if(rightTop[row+col+ queen.length-1])
+             if(rightTop[row+col])
              {continue;}
-             if(lefttTop[row+col])
+             if(lefttTop[row-col+ queen.length-1])
              {continue;}
              cols[col]=true;
-             rightTop[row+col+ queen.length-1]=true;
-             lefttTop[row+col]=true;
+             rightTop[row+col]=true;
+             lefttTop[row-col+ queen.length-1]=true;
+             queen[row]=col;
              place(row+1);
              cols[col]=false;
-             rightTop[row+col+ queen.length-1]=false;
-             lefttTop[row+col]=false;
+             rightTop[row+col]=false;
+             lefttTop[row-col+ queen.length-1]=false;
 
          }
     }
@@ -64,4 +72,19 @@ public class Queen8 {
 
 }
 //上面这个方法要有优化，没必要每次都循环上面的row一遍
+
+    //show方法，打印出当前皇后的排放情况
+    public void show(){
+        for (int row = 0; row <queen.length; row++) {
+            for (int col=0;col<queen.length;col++){
+                if(queen[row]==col){
+                    System.out.print("1  ");
+
+                }else{
+                    System.out.print("0  ");
+                }
+            }
+            System.out.println("");
+        }
+    }
 }
